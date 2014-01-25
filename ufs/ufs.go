0b4a35f8756437930b8a2d80b1aeda23e0471592
Ronald G. Minnich <rminnich@gmail.com>
2014-01-25 06:24:23 +0100

    Added ufs
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ ufs/ufs.go
@@ -0,0 +1,34 @@
+// Copyright 2009 The go9p Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"github.com/rminnich/go9p"
+	"flag"
+	"fmt"
+	"log"
+)
+
+var addr = flag.String("addr", ":5640", "network address")
+var debug = flag.Int("debug", 0, "print debug messages")
+var root = flag.String("root", "/", "root filesystem")
+
+func main() {
+	flag.Parse()
+	ufs := new(go9p.Ufs)
+	ufs.Dotu = true
+	ufs.Id = "ufs"
+	ufs.Root = *root
+	ufs.Debuglevel = *debug
+	ufs.Start(ufs)
+
+	fmt.Print("ufs starting\n");
+	// determined by build tags
+	//extraFuncs()
+	err := ufs.StartNetListener("tcp", *addr)
+	if err != nil {
+		log.Println(err)
+	}
+}
