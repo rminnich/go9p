a63558099553c77a0d64726ebc1569a12b7221eb
Fazlul Shahriar <fshahriar@gmail.com>
2018-01-25 11:01:35 -0500

    Fix Plan 9 build.

---
+++ akaros.go
@@ -0,0 +1,12 @@
+// Copyright 2018 The Go9p Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package go9p
+
+import "flag"
+
+/* enables "Akaros" capabilities, which right now means
+ * a sane error message format.
+ */
+var Akaros = flag.Bool("akaros", false, "Akaros extensions")
