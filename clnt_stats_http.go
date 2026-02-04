5e67be24776770da14f267a101669f964760da1c
Ronald Minnich <rminnich@gmail.com>
2026-02-04 15:19:58 -0700

    fix copyrights, make it work with tinygo
    Signed-off-by: Ronald Minnich <rminnich@gmail.com>

--- clnt_stats_http.go
+++ clnt_stats_http.go
@@ -1,5 +1,8 @@
-//go:build httpstats
-// +build httpstats
+// Copyright 2009 The Go9p Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build httpstats && !tinygo
 
 package go9p
 
